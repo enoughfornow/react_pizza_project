99ee8b88df59e108261cc54386dff77d766cab76
Dmitry <enoughfornowbusiness@gmail.com>
2022-08-09 17:12:12 +0300

    add rest api (mockapi) db

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import items from '../src/pizzas.json';
-
 import './scss/app.scss';
+import React from 'react';
 
 function App() {
+  const [items, setItems] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('https://62f2550618493ca21f317d6b.mockapi.io/react_project')
+    .then((response) => response.json())
+    .then((arr) => {
+      setItems(arr)
+    })
+  }, [])
+
   return (
     <div className="wrapper">
       <Header />
