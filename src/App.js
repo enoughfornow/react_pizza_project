7884ce55b913a3c3c1caf582ed917e73393a92d0
Dmitry <enoughfornowbusiness@gmail.com>
2022-08-09 20:17:59 +0300

    add navigation (react-router), pre-load skeleton component(react skeleton), few remarks

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';  
+
+import { Routes, Route } from 'react-router-dom';
 import './scss/app.scss';
 import React from 'react';
 
 function App() {
-  const [items, setItems] = React.useState([])
-
-  React.useEffect(() => {
-    fetch('https://62f2550618493ca21f317d6b.mockapi.io/react_project')
-    .then((response) => response.json())
-    .then((arr) => {
-      setItems(arr)
-    })
-  }, [])
+ 
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
-          </div>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
---
+++ src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './NotFoundBlock.module.scss'
+
+export const NotFoundBlock = () => {
+  return (
+    <div className={styles.root}>
+      <h1>Ничего не найдено</h1>
+      <p className={styles.description}>К сожалению мы не смогли ничего найти :(</p>
+    </div>
+  )
+}
+
+
+export default NotFoundBlock;
