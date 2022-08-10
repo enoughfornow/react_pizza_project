bd5ea215a73dc069a7c8c6618a38862ed4e74eb0
Dmitry <enoughfornowbusiness@gmail.com>
2022-08-10 19:55:54 +0300

    added category sorting and sorting by rating, alphabet, price

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,22 +8,28 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 function Home() {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(true);
+  const [sortId, setSortId] = React.useState({
+    name: 'популярности',
+    sortProperty: 'rating',
+  });
 
   React.useEffect(() => {
-    fetch('https://62f2550618493ca21f317d6b.mockapi.io/react_project')
+    setIsLoading(true);
+    fetch(`https://62f2550618493ca21f317d6b.mockapi.io/react_project?${categoryId > 0 ? `category=${categoryId}` : ''}&sortBy=${sortId.sortProperty}&order=desc`)
       .then((response) => response.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
-      window.scrollTo(0, 0);
-  }, []);
+    window.scrollTo(0, 0);
+  }, [categoryId, sortId]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onClickCategoryId={(index) => setCategoryId(index)} />
+        <Sort sortValue={sortId} onClickSortId={(index) => setSortId(index)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
