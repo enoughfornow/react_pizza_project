b04a7c259a26eeb07130d190b2f17bbc4f7d2639
Dmitry <enoughfornowbusiness@gmail.com>
2022-08-15 22:09:30 +0300

    day 4 change (use redux-toolkit)

--- src/components/Search/Index.jsx
+++ src/components/Search/Index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { SearchContext } from '../../App';
 
 import styles from './Search.module.scss';
 
-const Search = ({ searchValue, setSearchValue }) => {
+const Search = () => {
+
+  const { searchValue, setSearchValue } = React.useContext(SearchContext)
+
   return (
     <div className={styles.root}>
       <svg
@@ -43,8 +47,11 @@ const Search = ({ searchValue, setSearchValue }) => {
         placeholder="Search"
       />
       {searchValue && (
-        <svg onClick={() => setSearchValue('')}
-        className={styles.clearIcon} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          onClick={() => setSearchValue('')}
+          className={styles.clearIcon}
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg">
           <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
         </svg>
       )}
